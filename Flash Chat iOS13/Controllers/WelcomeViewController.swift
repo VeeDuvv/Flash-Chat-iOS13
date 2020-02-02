6f79733cb6c6c2207653b02d86e0cb7b5269316e
VeeDuvv <Vamsi.duvvuri@icloud.com>
2020-02-02 00:02:45 -0500

    connected Register View Controller to Segue

--- Flash Chat iOS13/Controllers/WelcomeViewController.swift
+++ Flash Chat iOS13/Controllers/WelcomeViewController.swift
@@ -7,15 +7,17 @@
 //
 
 import UIKit
+import CLTypingLabel
+import Firebase
 
 class WelcomeViewController: UIViewController {
 
-    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var titleLabel: CLTypingLabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
-       
+        titleLabel.text = "⚡︎ Flash Chat"
+        FirebaseApp.configure()
     }
     
 
